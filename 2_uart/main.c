238dc7e756f6620d1c7876879e055ab41ffb9306
tfossdal <125375459+tfossdal@users.noreply.github.com>
2023-03-30 14:38:46 +0200

    updated 30.03

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -1,8 +1,21 @@
 #include "uart.h"
+#include <stdio.h>
+#include <unistd.h>
 
+
+ssize_t _write(int fd, const void *buf, size_t count){
+    char * letter = (char *)(buf);
+    for(int i = 0; i < count; i++){
+        uart_send(*letter);
+        letter++;
+    }
+    return count;
+}
 int main(){
+    int ledOn = 0;
 
-    int sleep;
+    iprintf("The average grade in TTK%d was in %d and %d: %c\n\r",4235,2019,2018,'C');
+    
     while(1){
 
         if (!(GPIO0->IN & (1 << 23))){ // B_btn
@@ -12,8 +25,28 @@ int main(){
         uart_send('A');
         }
 
-        sleep = 10000;
-        while(--sleep);
+        if (UART0->EVENTS_RXDRDY){
+            if(!ledOn){
+
+                GPIO0->OUTSET = 1 << 21;
+			    GPIO0->OUTSET = 1 << 22;
+			    GPIO0->OUTSET = 1 << 15;
+			    GPIO0->OUTSET = 1 << 24;
+			    GPIO0->OUTSET = 1 << 19;
+                ledOn = 1;
+            }
+            else{
+                GPIO0->OUTCLR = 1 << 21;
+			    GPIO0->OUTCLR = 1 << 22;
+			    GPIO0->OUTCLR = 1 << 15;
+			    GPIO0->OUTCLR = 1 << 24;
+			    GPIO0->OUTCLR = 1 << 19;
+                ledOn = 0;
+            }
+
+        }
+
+       
     }
 
     return 0;
--- 2_uart/uart.h
+++ 2_uart/uart.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef UART_H
+#define UART_H
 #include "gpio.h"
 
 #define UART0 ((NRF_UART_REG*)0x40002000) //uart 0 base adress
@@ -14,30 +15,31 @@ typedef struct {
     volatile uint32_t EVENTS_CTS;
     volatile uint32_t EVENTS_NCTS;
     volatile uint32_t EVENTS_RXDRDY;
+    volatile uint32_t RESERVED[4];
     volatile uint32_t EVENTS_TXDRDY;
-    volatile uint32_t RESERVED2[5];
+    volatile uint32_t RESERVED3;
     volatile uint32_t EVENTS_ERROR;
-    volatile uint32_t RESERVED3[7];
+    volatile uint32_t RESERVED4[7];
     volatile uint32_t EVENTS_RXTO;
-    volatile uint32_t RESERVED4[46];
+    volatile uint32_t RESERVED5[46];
     volatile uint32_t SHORTS;
-    volatile uint32_t RESERVED5[64];
+    volatile uint32_t RESERVED6[64];
     volatile uint32_t INTENSET;
     volatile uint32_t INTENCLR;
-    volatile uint32_t RESERVED6[93];
+    volatile uint32_t RESERVED7[93];
     volatile uint32_t ERRORSRC;
-    volatile uint32_t RESERVED7[31];
+    volatile uint32_t RESERVED8[31];
     volatile uint32_t ENABLE;
-    volatile uint32_t RESERVED8;
+    volatile uint32_t RESERVED9;
     volatile uint32_t PSELRTS;
     volatile uint32_t PSELTXD;
     volatile uint32_t PSELCTS;
     volatile uint32_t PSELRXD;
     volatile uint32_t RXD;
     volatile uint32_t TXD;
-    volatile uint32_t RESERVED9;
+    volatile uint32_t RESERVED10;
     volatile uint32_t BAUDRATE;
-    volatile uint32_t RESERVED10[17];
+    volatile uint32_t RESERVED11[17];
     volatile uint32_t CONFIG;
 } NRF_UART_REG;
 
@@ -46,3 +48,5 @@ void uart_init();
 void uart_send(char letter);
 
 char uart_read();
+
+#endif
